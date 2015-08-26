e1c6b205c13e84810ae4240bdfdf7617607a7687
Alexander Pechkarev <alexpechkarev@gmail.com>
2015-08-26 17:09:49 +0100

    Create DefaultParameters.php

---
+++ src/ParamRoutine/DefaultParameters.php
@@ -0,0 +1,87 @@
+<?php namespace App\GoogleMaps\ParamRoutine;
+/**
+ * Description of DefaultParameters
+ *
+ * @author alex.petchkarev
+ */
+use App\GoogleMaps\GoogleMaps;
+
+class DefaultParameters{
+      
+    
+    /**
+     * Generate Service URL parameters string
+     * @param array $param
+     * @return string
+     */
+    public static function getQueryString( &$param ){
+        
+        // Geocoding components parameter
+        if( isset($param['components']) && is_array($param['components'])){
+           $param['components'] = GoogleMaps::joinParam( $param['components'], '=', '&');
+        }   
+        ///
+        
+        // Direction parameters
+        if( isset($param['origin']) && is_array($param['origin'])){
+           $param['origin'] = GoogleMaps::joinParam( $param['origin'], '', ':', false );
+        }   
+        
+        if( isset($param['destination']) && is_array($param['destination'])){
+           $param['destination'] = GoogleMaps::joinParam( $param['destination'], '', ':', false );
+        }      
+        
+        if( isset($param['waypoints']) && is_array($param['waypoints'])){
+           $param['waypoints'] = GoogleMaps::joinParam( $param['waypoints'], '', '|', false );
+        } 
+        
+        if( isset($param['avoid']) && is_array($param['avoid'])){
+           $param['avoid'] = GoogleMaps::joinParam( $param['avoid'], '', '|', false );
+        }     
+        ///
+        
+        // Distance Matrix parameters
+        if( isset($param['origins']) && is_array($param['origins'])){
+           $param['origins'] = GoogleMaps::joinParam( $param['origins'], '', '|', false );
+        }   
+        
+        if( isset($param['destinations']) && is_array($param['destinations'])){
+           $param['destinations'] = GoogleMaps::joinParam( $param['destinations'], '', '|', false );
+        }   
+        
+        if( isset($param['transit_mode']) && is_array($param['transit_mode'])){
+           $param['transit_mode'] = GoogleMaps::joinParam( $param['transit_mode'], '', '|', false );
+        } 
+        ///
+        
+        // Elevation & Road parameters
+        if( isset($param['locations']) && is_array($param['locations'])){
+           $param['locations'] = GoogleMaps::joinParam( $param['locations'], '', '|', false );
+        }   
+        if( isset($param['path']) && is_array($param['path'])){
+           $param['path'] = GoogleMaps::joinParam( $param['path'], '', '|', false );
+        } 
+        ///
+        
+        // Places & Time Zone parameters
+        if( isset($param['location']) && is_array($param['location'])){
+           $param['location'] = GoogleMaps::joinParam( $param['location'], '', ',', false );
+        } 
+        
+        if( isset($param['types']) && is_array($param['types'])){
+           $param['types'] = GoogleMaps::joinParam( $param['types'], '', '|', false );
+        }        
+        ///
+                
+        
+
+        return  GoogleMaps::joinParam( $param );
+
+    }
+    
+    
+   
+    
+}
+
+?>
